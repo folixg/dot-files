eb205a4d3f74f6f2422f786be188efb481181094
Thomas Goldbrunner <thomas.goldbrunner@posteo.de>
2017-05-02 17:46:03 +0200

    added single screen option to lock screen

--- i3/lock/lock.sh
+++ i3/lock/lock.sh
@@ -5,4 +5,7 @@ playerctl pause
 #i3lock -i ~/Firefox_wallpaper.png -t -f
 
 # fancy lock screen https://git.fleshless.org/misc/tree/i3lock-extra
-~/.i3/lock/i3lock-extra.sh -s -b -p -o ~/.i3/lock/dualscreen_locked.png 
+# single screen
+#~/.i3/lock/i3lock-extra.sh -s -b -p -o ~/.i3/lock/locked.png 
+## dual screen
+~/.i3/lock/i3lock-extra.sh -s -b -p -o ~/.i3/lock/dualscreen_locked.png
