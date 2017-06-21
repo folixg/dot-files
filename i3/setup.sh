0efb06b37231d902ee42e8acfcb2e62dcc37f4dc
Thomas Goldbrunner <thomas.goldbrunner@tum.de>
2017-06-21 21:50:10 +0200

    i3: added Source Code Pro installation to setup script

--- i3/setup.sh
+++ i3/setup.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
 # dependencies
-# - i3 : metapackage (i3 window manager, screen locker, menu, statusbar) 
+# - i3 : metapackage (i3 window manager, screen locker, menu, statusbar)
 # - i3blocks : used as i3bar status command
 # - feh : used to show custom background image
 # - xautolock: lock inactive session
 # - scrot: screenshot tool for fancy lock screen
 # - playerctl: mpris control for media players
+# 
+# fonts
+# - Source Code Pro
+# - FontAwesome
 
 # install everything that's available through apt
 packets="i3 i3blocks feh xautolock scrot"
@@ -17,13 +21,38 @@ wget https://github.com/acrisci/playerctl/releases/download/v0.5.0/playerctl-0.5
 sudo dpkg -i playerctl-0.5.0_amd64.deb
 rm playerctl-0.5.0_amd64.deb
 
-#install Font Awesome in user home
-if [ ! -d ~/.fonts ]; then
-  mkdir ~/.fonts
+#install fonts in user home
+if [ "$(fc-list | grep -c "Source Code Pro")" == "0" ]; then
+  install_scp="1"
+else
+  install_scp="0"
 fi
-git clone https://github.com/FortAwesome/Font-Awesome.git ~/.fonts/fa
-fc-cache -f -v .fonts/fa
-
+if [ "$(fc-list | grep -c "FontAwesome")" == "0" ]; then
+  install_fa="1"
+else
+  install_fa="0"
+fi
+echo $install_fa
+echo $install_scp
+if [[ $install_scp == "1" || $install_fa == "1" ]] ; then
+  # create user font dir, if it does not exist
+  if [ ! -d ~/.fonts ]; then
+    mkdir ~/.fonts
+  fi
+  cd ~/.fonts || exit
+  # install Source Code Pro
+  if [ $install_scp == "1" ] ; then
+    wget https://github.com/adobe-fonts/source-code-pro/archive/2.030R-ro/1.050R-it.tar.gz
+    tar xvf 1.050R-it.tar.gz
+    rm xvf 1.050R-it.tar.gz
+  fi
+  # install FontAwesome
+  if [ $install_fa == "1" ] ; then
+    git clone https://github.com/FortAwesome/Font-Awesome.git ~/.fonts/fa
+  fi
+  # update font cache
+  fc-cache -f -v .fonts/fa
+fi
 # link config files
 if [ ! -d ~/.i3 ]; then
   mkdir ~/.i3
