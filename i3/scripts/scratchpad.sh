c37776a768793bc86b1c255fae4f436618f9f1fb
Thomas Goldbrunner <thomas.goldbrunner@tum.de>
2018-02-27 10:01:47 +0100

    i3: include scratchpad launcher in config

--- i3/scripts/scratchpad.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-xterm -xrm 'XTerm.vt100.allowTitleOps: false' -T 'i3-scratchpad' \
-  -e vim -c 'set ft=markdown' -c 'set noswapfile' /tmp/scratchpad &
-sleep 0.2
-i3-msg [title="i3-scratchpad"] move scratchpad
