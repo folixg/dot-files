6ff38363d6d63982d23d6a3c2aa103af86c151a6
Thomas Goldbrunner <thomas.goldbrunner@posteo.de>
2020-02-21 09:21:21 +0100

    fish: fix messed up function description

--- config/fish/functions/ubuntu.fish
+++ config/fish/functions/ubuntu.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function ubuntu --wraps='docker pull folixg/ubuntu:latest && docker run --rm -it -e USER_UID=$UID -e USER_GID=$GID -v `pwd`:/home/folix/workspace folixg/ubuntu' --wraps='docker pull folixg/ubuntu:latest && docker run --rm -it -e USER_UID=$UID -e USER_GID=$GID -v (pwd):/home/folix/workspace folixg/ubuntu' --wraps='docker pull folixg/ubuntu:latest && docker run --rm -it -e USER_UID=(id -u) -e USER_GID=(id -g) -v (pwd):/home/folix/workspace folixg/ubuntu' --description 'alias ubuntu docker pull folixg/ubuntu:latest && docker run --rm -it -e USER_UID=(id -u) -e USER_GID=(id -g) -v (pwd):/home/folix/workspace folixg/ubuntu'
+function ubuntu --wraps='docker pull folixg/ubuntu:latest && docker run --rm -it -e USER_UID=(id -u) -e USER_GID=(id -g) -v (pwd):/home/folix/workspace folixg/ubuntu' --description 'alias ubuntu docker pull folixg/ubuntu:latest && docker run --rm -it -e USER_UID=(id -u) -e USER_GID=(id -g) -v (pwd):/home/folix/workspace folixg/ubuntu'
   docker pull folixg/ubuntu:latest && docker run --rm -it -e USER_UID=(id -u) -e USER_GID=(id -g) -v (pwd):/home/folix/workspace folixg/ubuntu $argv;
 end
