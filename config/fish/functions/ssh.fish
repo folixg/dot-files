8ce5b7d79de027d34bb64a9fe58d49d982f61a99
Thomas Goldbrunner <thomas.goldbrunner@posteo.de>
2022-02-15 08:43:17 +0100

    fish: reuse tmux session with ssh

--- config/fish/functions/ssh.fish
+++ config/fish/functions/ssh.fish
@@ -1,4 +1,8 @@
 # Defined in /tmp/fish.UaEAGa/ssh.fish @ line 1
 function ssh
-    /usr/bin/ssh -t $argv "tmux new 2>/dev/null || zsh 2>/dev/null || bash"
+  if test -z "$TMUX"
+    /usr/bin/ssh -t $argv "tmux new -A -s ssh 2>/dev/null || fish 2>/dev/null || zsh 2>/dev/null || bash"
+  else 
+    /usr/bin/ssh -t $argv "fish 2>/dev/null || zsh 2>/dev/null || bash"
+  end
 end
